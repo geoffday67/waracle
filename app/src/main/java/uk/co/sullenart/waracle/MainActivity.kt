6a5e7b848243beb420ca76bad8b74e8717e1dcac
Geoff Day <geoffday67@gmail.com>
2019-11-23 16:12:29 +0000

    Handle orientation change

--- app/src/main/java/uk/co/sullenart/waracle/MainActivity.kt
+++ app/src/main/java/uk/co/sullenart/waracle/MainActivity.kt
@@ -1,11 +1,19 @@
 package uk.co.sullenart.waracle
 
+import android.content.res.Configuration
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import timber.log.Timber
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onConfigurationChanged(newConfig: Configuration) {
+        super.onConfigurationChanged(newConfig)
+        // TODO Add better config change handling, e.g. by saving and restoring instance data
+        Timber.d("Config")
     }
 }
